642a33a639160491df03930ef5b6313de842418a
rockybala <rgarg@cern.ch>
2023-03-24 18:47:48 +0100

    username changes

--- xAHAnalysis/run/LocalRun_TestStudies.sh
+++ xAHAnalysis/run/LocalRun_TestStudies.sh
@@ -3,28 +3,27 @@ extra=""
 #extra="--inputList"
 
 # this  has to be somewhere you can write
-parentsubmitdir="/afs/cern.ch/work/e/ebusch/public/SVJ/ana-exot-2021-19/xAHAnalysis/run/"
+parentsubmitdir="/afs/cern.ch/work/r/rgarg/public/Analyses/SemiVisibleJets/NTupleMaker/ana-exot-2021-19/xAHAnalysis/run/"
 
 ## Signal
-infile="/eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/DAODs/mc20_13TeV.515499.MGPy8EG_SVJSChan2j_2000_2.deriv.DAOD_PHYS.e8461_s3995_r13167_p5440/DAOD_PHYS.31295849._000001.pool.root.1"
-configFile=../src/SVJAlgo/data/config_SVJAlgo_signal.py
+#infile="/eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/DAODs/mc20_13TeV.515499.MGPy8EG_SVJSChan2j_2000_2.deriv.DAOD_PHYS.e8461_s3995_r13167_p5440/DAOD_PHYS.31295849._000001.pool.root.1"
+#configFile=../src/SVJAlgo/data/config_SVJAlgo_signal.py
 
 ## Multijet
 #infile="/eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/DAODs/mc20_13TeV.364706.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ6WithSW.deriv.DAOD_PHYS.e7142_s3681_r13145_p5440/DAOD_PHYS.31204957._000013.pool.root.1"
-#configFile=../src/SVJAlgo/data/config_SVJAlgo_bkg.py
+infile="/afs/cern.ch/work/r/rgarg/public/Analyses/SemiVisibleJets/NTupleMaker/ana-exot-2021-19/mc20_13TeV.700324.Sh_2211_Zmumu_maxHTpTV2_CFilterBVeto.deriv.DAOD_PHYS.e8351_s3681_r13167_p5440/DAOD_PHYS.31209714._000031.pool.root.1"
+configFile=../src/SVJAlgo/data/config_SVJAlgo_bkg.py
 
 ## Data
 #infile="/eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/data/data17_13TeV.00324320.physics_Main.deriv.DAOD_PHYS.r13286_p4910_p5440/DAOD_PHYS.31140221._000023.pool.root.1"
 #configFile=../src/SVJAlgo/data/config_SVJAlgo_Nominal_2018.py
 
-
-
-nevents=1000
-submitdir=SIG_to_TTREE_${nevents}
+nevents=50000
+submitdir=Bkg_QCD_${nevents}
 
 
 echo "xAH_run.py --files ${infile} --nevents $nevents --config $configFile --extraOptions="" --submitDir ${parentsubmitdir}/$submitdir --log-level $loglevel $extra --force direct "
-xAH_run.py --files ${infile} --nevents $nevents --config $configFile --extraOptions="" --submitDir ${parentsubmitdir}/$submitdir --log-level $loglevel $extra --force direct 
+xAH_run.py --files ${infile} --nevents $nevents --config $configFile --extraOptions="" --submitDir ${parentsubmitdir}/$submitdir --log-level $loglevel $extra --force direct
 
 # test command for using rucio with file list
 #filelisttxt="/afs/cern.ch/user/m/mamerl/TLAWork/inputlisttest.txt"
--- xAHAnalysis/run/grid_run.sh
+++ xAHAnalysis/run/grid_run.sh
@@ -2,8 +2,11 @@ loglevel=info
 
 # this  has to be somewhere you can write
 #parentsubmitdir="/afs/cern.ch/user/j/jodavies/darkjetresonances/DAODtoNTUPLE/run/"
-parentsubmitdir="/afs/cern.ch/work/e/ebusch/public/SVJ/ana-exot-2021-19/xAHAnalysis/run/"
-WorkDir_DIR="/afs/cern.ch/work/e/ebusch/public/SVJ/ana-exot-2021-19/xAHAnalysis"
+#parentsubmitdir="/afs/cern.ch/work/e/ebusch/public/SVJ/ana-exot-2021-19/xAHAnalysis/run/"
+#WorkDir_DIR="/afs/cern.ch/work/e/ebusch/public/SVJ/ana-exot-2021-19/xAHAnalysis"
+# changing to my area
+parentsubmitdir="/afs/cern.ch/work/r/rgarg/public/Analyses/SemiVisibleJets/NTupleMaker/ana-exot-2021-19/xAHAnalysis/run/"
+WorkDir_DIR="/afs/cern.ch/work/r/rgarg/public/Analyses/SemiVisibleJets/NTupleMaker/ana-exot-2021-19/xAHAnalysis/"
 configFile=$WorkDir_DIR/src/SVJAlgo/data/config_SVJAlgo_bkg.py
 sigconfigFile=$WorkDir_DIR/src/SVJAlgo/data/config_SVJAlgo_signal.py
 
@@ -15,8 +18,8 @@ do
 	do
 		inputFile=${BKG}_${MC}.txt
 		echo $inputFile
-		echo "xAH_run.py --inputRucio --inputList --files $inputFile --config $configFile --extraOptions="" --force --submitDir $submitdir prun --optBatchShellInit 'source $WorkDir_DIR/setup.sh' --optGridOutputSampleName=user.ebusch.%in:name[2]%.%in:name[3]%.v6c.${MC} --optGridNGBPerJob=2"
-		xAH_run.py --inputRucio --inputList --files $inputFile --config $configFile --extraOptions="" --force --submitDir $submitdir prun --optBatchShellInit 'source $WorkDir_DIR/setup.sh' --optGridOutputSampleName=user.ebusch.%in:name[2]%.%in:name[3]%.v6c.${MC} --optGridNGBPerJob=2
+		echo "xAH_run.py --inputRucio --inputList --files $inputFile --config $configFile --extraOptions="" --force --submitDir $submitdir prun --optBatchShellInit 'source $WorkDir_DIR/setup.sh' --optGridOutputSampleName=user.rgarg.%in:name[2]%.%in:name[3]%.v6c.${MC} --optGridNGBPerJob=2"
+		xAH_run.py --inputRucio --inputList --files $inputFile --config $configFile --extraOptions="" --force --submitDir $submitdir prun --optBatchShellInit 'source $WorkDir_DIR/setup.sh' --optGridOutputSampleName=user.rgarg.%in:name[2]%.%in:name[3]%.v6c.${MC} --optGridNGBPerJob=2
 	done
 done
 
@@ -27,8 +30,8 @@ do
 	do
 		inputFile=${SIG}_${MC}.txt
 		echo $inputFile
-		echo "xAH_run.py --inputRucio --inputList --files $inputFile --config $sigconfigFile --extraOptions="" --force --submitDir $submitdir prun $PUfile --optBatchShellInit 'source $WorkDir_DIR/setup.sh' --optGridOutputSampleName=user.ebusch.%in:name[2]%.%in:name[3]%.v6c.${MC} --optGridNGBPerJob=2"
-		xAH_run.py --inputRucio --inputList --files $inputFile --config $sigconfigFile --extraOptions="" --force --submitDir $submitdir prun --optBatchShellInit 'source $WorkDir_DIR/setup.sh' --optGridOutputSampleName=user.ebusch.%in:name[2]%.%in:name[3]%.v6c.${MC} --optGridNGBPerJob=2
+		echo "xAH_run.py --inputRucio --inputList --files $inputFile --config $sigconfigFile --extraOptions="" --force --submitDir $submitdir prun $PUfile --optBatchShellInit 'source $WorkDir_DIR/setup.sh' --optGridOutputSampleName=user.rgarg.%in:name[2]%.%in:name[3]%.v6c.${MC} --optGridNGBPerJob=2"
+		xAH_run.py --inputRucio --inputList --files $inputFile --config $sigconfigFile --extraOptions="" --force --submitDir $submitdir prun --optBatchShellInit 'source $WorkDir_DIR/setup.sh' --optGridOutputSampleName=user.rgarg.%in:name[2]%.%in:name[3]%.v6c.${MC} --optGridNGBPerJob=2
 	done
 done
 
