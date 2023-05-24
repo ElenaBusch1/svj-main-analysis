2618bdea4b517c1cdf5e95ec099d67533bce7cf3
ebusch <ebusch@cern.ch>
2023-05-24 14:40:43 +0200

    another 2d pltos

--- Results/myPlotter.C
+++ Results/myPlotter.C
@@ -44,7 +44,7 @@ void myPlotter(){
 	class MicroNTuplePlotter plotter( filetags, path );
 
         //***************** Plot General options *******************// 
-        plotter.plot_norm_full = true;
+        plotter.plot_norm = false;
         plotter.plot_log = true;
         plotter.plot_log_ratio = false;
 	plotter.plot_error = true;
@@ -91,14 +91,14 @@ void myPlotter(){
 	//plotter.PlotOverlay("");
 
 	//plotter.SetPlots ( plot_all );
-	plotter.SetPlots ( {P_jet1_pt} );
+	//plotter.SetPlots ( {P_jet1_pt} );
 	//plotter.SaveOutputFile("test");
         //plotter.SetPlots ( { P_jet1_DL1dv01, P_jet2_DL1dv01, P_jet1_GN1, P_jet2_GN1} );
 	//plotter.SetPlots( {P_dphi_min_MET, P_mT_jj_neg, P_met_jj_neg, P_dphi_MET_j1j2});
 	//plotter.SetPlots ( {P_mT_jj, P_jet1_pt} );
-	plotter.Plot("ratio");
+	//plotter.Plot("ratio");
 
         //***************** 2D Plot *******************// 	
 	//plotter.Plot2D(P_jet1_phi, P_met_phi);
-	//plotter.Plot2D(P_jet1_pt, P_met_met);
+	plotter.Plot2D(P_jet1_pt, P_met_met);
 }
